8af2bf86073bb2cd5d8ed826d8fe24da48fe7892
CamRussell <cameraha@gmail.com>
2019-11-29 14:32:59 +1300

    Added 'background color'

--- Pong/src/Pong.java
+++ Pong/src/Pong.java
@@ -2,12 +2,18 @@ import javax.swing.JFrame;
  
 public class Pong extends JFrame {
 	
+	private final static String WINDOW_TITLE = "Pong";
+    private final static int WINDOW_WIDTH = 800;
+    private final static int WINDOW_HEIGHT = 600;
+    
+	
 	public Pong() {
-    	setTitle("Pong");
-    	setSize(100, 200);
-    	setResizable(true);
-    	setVisible(true);
-    	setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setTitle(WINDOW_TITLE);
+		setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+        setResizable(false);
+        setVisible(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        add(new PongPanel());
 }
  
 	public static void main(String[] args) {
